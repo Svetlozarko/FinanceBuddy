c940e6758f1c5e805315c94d8d56e9116e2587c1
Svetlozar Trifonov <svetlozartrifonov60@gmail.com>
2025-06-17 12:02:12 +0300

    Added articles ( 3 )

---
+++ FinanceCalc/Controllers/ArticlesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceCalc.Controllers
+{
+    [Route("Articles")]
+    public class ArticlesController : Controller
+    {
+        [HttpGet("{slug}")]
+        public IActionResult Article(string slug)
+        {
+            return slug switch
+            {
+                "article1" => View("Article1"),
+                "article2" => View("Article2"),
+                "article3" => View("Article3"),
+                _ => NotFound()
+            };
+        }
+    }
+}
