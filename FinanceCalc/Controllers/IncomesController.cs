56a17db91dbec4c1ee4b64404693606ff61d9275
Svetlozar Trifonov <svetlozartrifonov60@gmail.com>
2025-06-22 16:48:46 +0300

    Corrected IncomeVsExpenses section.

--- FinanceCalc/Controllers/IncomesController.cs
+++ FinanceCalc/Controllers/IncomesController.cs
@@ -4,6 +4,7 @@ using FinanceCalc.Models;
 using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinanceCalc.Controllers
 {
@@ -28,19 +29,37 @@ namespace FinanceCalc.Controllers
             if (userId == null)
                 return Unauthorized();
 
+            // Assign userId to income object
             income.UserId = userId;
-            income.Amount = income.Amount;
 
-            _context.Add(income);
+            // Try to find an existing income record for this user
+            var existingIncome = await _context.Income
+                .FirstOrDefaultAsync(i => i.UserId == userId);
+
+            if (existingIncome != null)
+            {
+                // Update the existing income amount
+                existingIncome.Amount = income.Amount;
+
+                // Mark the entity as modified (optional if tracked)
+                _context.Income.Update(existingIncome);
+            }
+            else
+            {
+                // No existing record, add new
+                _context.Income.Add(income);
+            }
+
             await _context.SaveChangesAsync();
 
             return Ok(new
             {
                 success = true,
-                income.Id,
-                income.Amount,
+                incomeId = existingIncome?.Id ?? income.Id,
+                amount = existingIncome?.Amount ?? income.Amount,
             });
         }
+
 
     }
 }
